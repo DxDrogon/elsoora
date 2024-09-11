93f7d2afed7aa80aa211dd7cbcef24e478642538
SzDominik <144907739+DxDrogon@users.noreply.github.com>
2024-09-11 09:06:24 +0200

    Delete engine.js

--- engine.js
+++
@@ -1,9 +0,0 @@
-const champs = ['Briar', 'Darius', 'Aurora']
-
-function init() {
-    document.getElementById('body').innerHTML = champs.map(c =>
-        `<div onclick="f('${c}')">${c}</div>`
-    ).join("")
-}
-
-const f = console.log
